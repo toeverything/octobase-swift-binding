c24760b27657d9f53d8ba2782df522aada201c2c
x1a0t <405028157@qq.com>
2023-03-13 18:47:26 +0800

    fix: wrong octobase.podspec path for push_tag.sh

--- scripts/push_tag.sh
+++ scripts/push_tag.sh
@@ -3,7 +3,7 @@
 # https://github.com/toeverything/octobase-swift-binding.git
 # TODO automatically bump version in octobase.podspec
 
-podspec_path="octobase.podspec" &&
+podspec_path="../octobase.podspec" &&
 version=$(awk -F'"' '/spec.version/ {print $2; exit;}' "${podspec_path}") &&
 version_with_prefix="swift-v${version}" &&
 git tag "${version_with_prefix}" &&
