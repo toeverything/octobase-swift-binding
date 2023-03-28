cdcb87f5396f42624345de45e4dcd9966250eedc
x1a0t <405028157@qq.com>
2023-03-28 16:32:38 +0800

    ci: add publishing swift package workflow

--- scripts/bump-podspec-version.sh
+++ scripts/bump-podspec-version.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-podspec=$(cat ../octobase.podspec)
+podspec=$(cat ./octobase.podspec)
 current_version=$(echo "$podspec" | grep -oE 'spec\.version\s*=\s*"[0-9]+\.[0-9]+\.[0-9]+"' | awk -F'"' '{print $2}')
 
 IFS='.' read -ra version_parts <<< "$current_version"
@@ -10,4 +10,4 @@ patch=$((version_parts[2] + 1))
 
 new_version="$major.$minor.$patch"
 new_podspec=$(echo "$podspec" | sed "s/$current_version/$new_version/")
-echo "$new_podspec" > ../octobase.podspec
+echo "$new_podspec" > ./octobase.podspec
--- scripts/push-tag.sh
+++ scripts/push-tag.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 # This script extracts the version from octobase.podspec as git tag, and push to
 # https://github.com/toeverything/octobase-swift-binding.git
-# TODO automatically bump version in octobase.podspec
 
-podspec_path="../octobase.podspec" &&
+podspec_path="./octobase.podspec" &&
 version=$(awk -F'"' '/spec.version/ {print $2; exit;}' "${podspec_path}") &&
 version_with_prefix="swift-v${version}" &&
 git tag "${version_with_prefix}" &&
